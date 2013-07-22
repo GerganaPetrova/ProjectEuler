2de448ed9a8e14b0cae8d7f5c752be16c3deb5f1
GerganaPetrova <josephin.bella@gmail.com>
2013-07-22 15:13:33 +0300

    add problem_21

---
+++ problem_21.py
@@ -0,0 +1,17 @@
+from math import sqrt
+
+def d(n):
+    s = 1
+    for i in range(2, int(sqrt(n) + 1)):
+        if n % i == 0:
+            s += i + n / i
+    return s
+
+result = 0
+for i in range(2,10000):
+    a = d(i)
+    if a > i and d(a) == i:
+        result += a + i
+print result 
+
+ 
