4f2add42258b1867d141b137175277cf90224aa1
GerganaPetrova <josephin.bella@gmail.com>
2013-07-22 11:06:35 +0300

    update description

--- problem_15.py
+++ problem_15.py
@@ -1,7 +1,7 @@
 from math import factorial
 
 #The number of how many routes we have can be found by
-#found by finding how many combinations of 20 right moves we can have in our 40 moves:
+#finding how many combinations of 20 right moves we can have in our 40 moves:
 def count_paths():
     return factorial(40) / (factorial(20) * factorial(20))
 
